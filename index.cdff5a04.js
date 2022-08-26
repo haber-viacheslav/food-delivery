d9a77b59ad44e228b45b682725d2e8dbaa6b06e2
jokercs <gaberslavik@gmail.com>
2022-08-26 07:18:24 +0000

    Deploying to gh-pages from @ jokercs/food-delivery@b25f8fd50836ad4763bb21a1d269e725ad4a4d1c 🚀

---
+++ index.cdff5a04.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-open]"),n=document.querySelector("[data-menu]");function t(){n.classList.toggle("is-open")}document.querySelector("body"),e.addEventListener("click",t),menuCloseBtn.addEventListener("click",t)}();
+//# sourceMappingURL=index.cdff5a04.js.map
---
+++ index.e9d97dd4.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-menu-open]"),t=document.querySelector("[data-menu]");document.querySelector("body");function n(){t.classList.toggle("is-open")}e.addEventListener("click",n),menuCloseBtn.addEventListener("click",n)})();
+//# sourceMappingURL=index.e9d97dd4.js.map
