70f96533b50c4852485cb23b977716f2f6bb122e
Viacheslav Haber <gaberslavik@gmail.com>
2022-08-29 14:36:46 +0300

    added changes

--- src/index.js
+++ src/index.js
@@ -82,6 +82,16 @@ const swiperCustomers = new Swiper('.swiper-customers', {
     el: '.swiper-pagination',
     clickable: true,
   },
+
+  effect: 'cube',
+
+  cubeEffect: {
+    slideShadows: false,
+    shadow: false,
+    ShadowOffset: 0,
+    shadowScale: 0,
+  },
+
   autoplay: {
     delay: 6000,
   },
