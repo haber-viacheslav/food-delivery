dadc1bfaea77ec7af6473b608445f5d4a172521e
Viacheslav Haber <gaberslavik@gmail.com>
2022-08-29 10:59:16 +0300

    added animate.css

--- src/index.js
+++ src/index.js
@@ -13,6 +13,25 @@
   }
 })();
 
+// Modal
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    body: document.querySelector('[data-page]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
+
 // Swiper module
 const swiper = new Swiper('.swiper', {
   // Accessibility
